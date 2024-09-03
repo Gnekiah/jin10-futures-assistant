4a2c88d81ec57a5073fb1f096e4e0031fafcd120
gnekiah <gnekiah@gmail.com>
2024-09-03 08:36:21 +0800

    [feat]: add block list
    -

--- jin10-futures-assistant.js
+++ jin10-futures-assistant.js
@@ -18,10 +18,10 @@
         }
     });
 
-    let whitelist_keywords = [
+    var whitelist_keywords = [
         "CFTC"
     ];
-    let blacklist_keywords = [
+    var blacklist_keywords = [
         "期货盯盘神器",
         "交易夜读",
         "订单流5分钟图显示",
@@ -31,26 +31,42 @@
         "资金炸弹复盘",
         "品种交易逻辑"
     ];
-    let blacklist_image_links = [
+    var blacklist_image_links = [
         "https://cdn-news.jin10.com/assets/common/13679271-9e9e-4b4f-8143-03a5524c78e9.jpg",
         "https://cdn-news.jin10.com/assets/common/33e9e3d7-9484-47e7-bed2-f64d7f2c4e48.jpg",
-        "https://flash-scdn.jin10.com/1375ec78-c99c-4aa1-a6bc-496d5d25df4c.png"
+        "https://cdn-news.jin10.com/assets/common/52f3ddff-d53c-488a-8370-9f62d68516f6.jpg",
+        "https://cdn-news.jin10.com/assets/common/40d04d52-951b-4132-a0b9-7474ae814efe.jpg",
+        "https://cdn-news.jin10.com/assets/common/e6bcdc1e-a911-4346-be23-5261c5f5156d.jpg",
+        "https://cdn-news.jin10.com/assets/common/c0624356-4e17-4136-a4d4-f50801f82b30.jpg",
+        "https://cdn-news.jin10.com/assets/common/317c5dce-0065-47d5-af04-6bd9a6a210a5.jpg",
+        "https://cdn-news.jin10.com/assets/common/73d61e20-bb54-44cc-87fa-43a9defc308f.jpg",
+        "https://cdn-news.jin10.com/assets/common/2d235cbd-f851-4e40-b16b-f72bdd72a7fb.jpg",
+        "https://cdn-news.jin10.com/assets/common/7ac16be9-232f-4279-bc33-5040a8a97862.jpg",
+        "https://cdn-news.jin10.com/assets/common/17659d35-bb8b-4f59-ab97-4c2f7c8567a0.jpg",
+        "https://cdn-news.jin10.com/assets/common/4c118b32-334f-4287-b41b-8a0d1177598d.jpg",
+        "https://cdn-news.jin10.com/assets/common/e6bcdc1e-a911-4346-be23-5261c5f5156d.jpg",
+        "https://cdn-news.jin10.com/assets/common/1e9e7eb1-cd6e-4609-8df1-57b69f5c8470.jpg",
+        "https://cdn-news.jin10.com/assets/common/41195501-edba-4e8d-b4e5-9887c338c45c.jpg",
+        "https://cdn-news.jin10.com/assets/common/978fa203-b4b0-4e19-a4dc-b5fe89cc7e68.jpg",
+        "https://cdn-news.jin10.com/assets/common/318ef326-a414-4cb2-b89d-d51ab6e6350f.jpg",
+        "https://cdn-news.jin10.com/assets/common/fd578307-fa2a-4b19-8551-73657c357bcd.jpg",
+        "https://cdn-news.jin10.com/assets/common/aa0df400-b3d9-4412-8921-6664d82f3f3a.jpg",
+        "https://flash-scdn.jin10.com/1375ec78-c99c-4aa1-a6bc-496d5d25df4c.png",
+        "https://flash-scdn.jin10.com/85194ea0-b68c-49bd-8c61-c1208a3dac06.jpg"
     ];
 
     var g_last_time_id = "";
+    var g_observer_list = [];
 
+    // 隐藏header、侧边栏、水印
     const es_ushk_header = document.getElementsByClassName("ushk-header");
-    while(es_ushk_header.length > 0){
-        es_ushk_header[0].parentNode.removeChild(es_ushk_header[0]);
-    }
+    es_ushk_header[0].setAttribute("style", "visibility: hidden; height: 0px; width: 0px; margin: 0px; padding: 0px; display: none");
+
     const es_ushk_side = document.getElementsByClassName("ushk-side");
-    while(es_ushk_side.length > 0){
-        es_ushk_side[0].parentNode.removeChild(es_ushk_side[0]);
-    }
+    es_ushk_side[0].setAttribute("style", "visibility: hidden; height: 0px; width: 0px; margin: 0px; padding: 0px; display: none");
+
     const es_watermark = document.getElementsByClassName("watermark");
-    while(es_watermark.length > 0){
-        es_watermark[0].parentNode.removeChild(es_watermark[0]);
-    }
+    es_watermark[0].setAttribute("style", "visibility: hidden; height: 0px;  width: 0px; margin: 0px; padding: 0px; display: none");
 
     // 添加一个按钮用来跳转
     const ushk_news_handle = document.getElementsByClassName("ushk-news__handle")[0];
@@ -64,121 +80,125 @@
     };
     ushk_news_handle.appendChild(last_stop_button);
 
+    // 关键词匹配
     function find_keyword_in_list(keywords, text) {
-        for (let i = 0; i < keywords.length; i++) {
-            if (text.indexOf(keywords[i]) > -1) {
+        for (const k of keywords) {
+            if (text.indexOf(k) > -1) {
                 return 1;
             }
         }
         return -1;
     }
 
+    // 对每条消息div添加一个点击事件，用来更新跳转信息
     function ushk_set_item_id(target_node) {
         target_node.onclick = function() {
-            const ushk_flash_time = target_node.getElementsByClassName("ushk-flash_time")[0].innerText.replaceAll(":", "-");
-            g_last_time_id = ushk_flash_time;
+            g_last_time_id = target_node.getElementsByClassName("ushk-flash_time")[0].innerText.replaceAll(":", "-");
             target_node.setAttribute("id", "g_last_stop_id_" + g_last_time_id);
             last_stop_button = document.getElementById("last_stop_button");
             last_stop_button.value = "跳转到上次标记：" + g_last_time_id;
         };
     }
 
+    // 处理全部消息
     function ushk_list_handler(target_node) {
         const ushk_flash_item_J_flash_item = target_node.getElementsByClassName("ushk-flash_item J_flash_item");
-        for(let i = 0; i < ushk_flash_item_J_flash_item.length; i++) {
-            const text = ushk_flash_item_J_flash_item[i].innerText;
+        for(const div of ushk_flash_item_J_flash_item) {
+            const text = div.innerText;
             // 先过滤关键词白名单，再过滤关键词黑名单
             if (find_keyword_in_list(whitelist_keywords, text) > -1) {
-                ushk_flash_item_J_flash_item[i].setAttribute("style", "padding-top: 5px");
-                ushk_set_item_id(ushk_flash_item_J_flash_item[i]);
+                div.setAttribute("style", "padding-top: 5px");
+                ushk_set_item_id(div);
             } else if (find_keyword_in_list(blacklist_keywords, text) > -1) {
                 console.log(text);
-                ushk_flash_item_J_flash_item[i].parentNode.removeChild(ushk_flash_item_J_flash_item[i]);
-                i--;
+                div.setAttribute("style", "visibility: hidden; height: 0px;  width: 0px; margin: 0px; padding: 0px; display: none");
             } else {
-                ushk_flash_item_J_flash_item[i].setAttribute("style", "padding-top: 5px");
-                ushk_set_item_id(ushk_flash_item_J_flash_item[i]);
+                div.setAttribute("style", "padding-top: 5px");
+                ushk_set_item_id(div);
             }
         }
+        //const ushk_flash_h = target_node.getElementsByClassName("ushk-flash_h");
+        //for (const div of ushk_flash_h) {
+        //    div.setAttribute("style", "padding: 0px");
+        //}
+        //const ushk_flash_b = target_node.getElementsByClassName("ushk-flash_b");
+        //for (const div of ushk_flash_b) {
+        //    div.setAttribute("style", "padding: 0px");
+        //}
+
         const ushk_flash_data_b = target_node.getElementsByClassName("ushk-flash_data_b");
-        for(let i = ushk_flash_data_b.length - 1; i >= 0; i--) {
-            ushk_flash_data_b[i].parentNode.removeChild(ushk_flash_data_b[i]);
+        for (const div of ushk_flash_data_b) {
+            div.setAttribute("style", "visibility: hidden; height: 0px;  width: 0px; margin: 0px; padding: 0px; display: none");
         }
+
         const dingpan_remark_bottom = target_node.getElementsByClassName("dingpan-remark_bottom");
-        for(let i = dingpan_remark_bottom.length - 1; i >= 0; i--) {
-            dingpan_remark_bottom[i].parentNode.removeChild(dingpan_remark_bottom[i]);
+        for (const div of dingpan_remark_bottom) {
+            div.setAttribute("style", "visibility: hidden; height: 0px;  width: 0px; margin: 0px; padding: 0px; display: none");
         }
+
         const imgtags = target_node.getElementsByTagName("img");
-        for (var i = imgtags.length - 1; i >= 0; i--) {
-            if (find_keyword_in_list(blacklist_image_links, imgtags[i].src) > -1) {
-                imgtags[i].parentNode.removeChild(imgtags[i]);
+        for (const img of imgtags) {
+            console.log(img.src);
+            if (find_keyword_in_list(blacklist_image_links, img.src) > -1) {
+                img.setAttribute("style", "visibility: hidden; height: 0px;  width: 0px; margin: 0px; padding: 0px; display: none");
             }
         }
     }
 
-    setTimeout(function(){
+    setTimeout(function() {
         const es_ushk_page = document.getElementsByClassName("ushk-page");
-        for(let i = 0; i < es_ushk_page.length; i++) {
-            es_ushk_page[i].setAttribute("style", "margin-top: 0px");
+        for(const div of es_ushk_page) {
+            div.setAttribute("style", "margin-top: 0px");
         }
         const es_ushk_wrap_main = document.getElementsByClassName("ushk-wrap ushk-wrap-main");
-        for(let i = 0; i < es_ushk_wrap_main.length; i++) {
-            es_ushk_wrap_main[i].setAttribute("style", "width: auto; margin-left: 0px; margin-right: 0px");
+        for(const div of es_ushk_wrap_main) {
+            div.setAttribute("style", "width: auto; margin-left: 0px; margin-right: 0px");
         }
         const ushk_headline_top = document.getElementsByClassName("ushk-main ushk-headline-top");
-        for(let i = 0; i < ushk_headline_top.length; i++) {
-            ushk_headline_top[i].setAttribute("style", "margin-right: 0px;padding: 0px; margin-bottom: 0px; display: none");
+        for(const div of ushk_headline_top) {
+            div.setAttribute("style", "margin-right: 0px;padding: 0px; margin-bottom: 0px; display: none");
         }
         const headline_top = document.getElementsByClassName("headline-top");
-        for(let i = 0; i < headline_top.length; i++) {
-            headline_top[i].setAttribute("style", "margin-bottom: 0px; padding: 0px; display: none");
+        for(const div of headline_top) {
+            div.setAttribute("style", "margin-bottom: 0px; padding: 0px; display: none");
         }
         const es_ushk_main = document.getElementsByClassName("ushk-main");
-        for(let i = 0; i < es_ushk_main.length; i++) {
-            es_ushk_main[i].setAttribute("style", "margin-right: 0px; padding: 0px");
+        for(const div of es_ushk_main) {
+            div.setAttribute("style", "margin-right: 0px; padding: 0px");
         }
-        const es_channel_list = document.getElementsByClassName("channel-list");
-        const es_channel_list_li = es_channel_list[0].getElementsByTagName("li");
-        for(let i = 0; i < es_channel_list_li.length; i++) {
-            es_channel_list_li[i].setAttribute("style", "flex: 6% 0 0");
+        const es_channel_list_li = document.getElementsByClassName("channel-list")[0].getElementsByTagName("li");
+        for(const li of es_channel_list_li) {
+            li.setAttribute("style", "flex: 6% 0 0");
         }
-        const es_channel_list_span = es_channel_list[0].getElementsByTagName("span");
-        for(let i = 0; i < es_channel_list_span.length; i++) {
-            es_channel_list_span[i].setAttribute("style", "padding: 0px");
+        const es_channel_list_span = document.getElementsByClassName("channel-list")[0].getElementsByTagName("span");
+        for(const span of es_channel_list_span) {
+            span.setAttribute("style", "padding: 0px");
         }
 
         const ushk_group = document.getElementsByClassName("ushk-flash__group J_flash_group")[0];
         const ushk_group_parent = ushk_group.parentNode;
         ushk_list_handler(ushk_group_parent);
 
-        var observer_list = [];
-
+        // 添加消息更新的监听事件
         new MutationObserver((mutationsList, self) => {
-            for(const mutation of mutationsList) {
-                if(mutation.type === "childList"){
-                    ushk_list_handler(ushk_group);
-                }
-            }
+            ushk_list_handler(ushk_group);
         }).observe(ushk_group, { childList: true });
-        observer_list.push(ushk_group);
+        g_observer_list.push(ushk_group);
 
+        // 添加包含history更新的监听事件
         new MutationObserver((mutationsList, self) => {
-            for(const mutation of mutationsList) {
-                const ushk_group_history = document.getElementsByClassName("ushk-flash__group J_flash_group historyDate");
-                for (let i = 0; i < ushk_group_history.length; i++) {
-                    ushk_list_handler(ushk_group_history[i]);
-                    if (observer_list.indexOf(ushk_group_history[i]) == -1) {
-                        new MutationObserver((mutationsList, self) => {
-                            for(const mutation of mutationsList) {
-                                ushk_list_handler(ushk_group_history[i]);
-                            }
-                        }).observe(ushk_group_history[i], { childList: true });
-                        observer_list.push(ushk_group_history[i]);
-                    }
+            const ushk_group_history = document.getElementsByClassName("ushk-flash__group J_flash_group historyDate");
+            for (const div of ushk_group_history) {
+                ushk_list_handler(div);
+                if (g_observer_list.indexOf(div) == -1) {
+                    new MutationObserver((mutationsList, self) => {
+                        ushk_list_handler(div);
+                    }).observe(div, { childList: true });
+                    g_observer_list.push(div);
                 }
             }
         }).observe(ushk_group_parent, { childList: true });
-        observer_list.push(ushk_group_parent);
+        g_observer_list.push(ushk_group_parent);
 
         // 默认将列表第一条消息设置为标记位置
         const ushk_group_top_item = ushk_group.getElementsByClassName("ushk-flash_item J_flash_item")[0];
