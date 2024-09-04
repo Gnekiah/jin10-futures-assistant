3b1d65bc7661f57e914a86dfe4654f7e61ab08bf
gnekiah <gnekiah@gmail.com>
2024-09-04 14:12:43 +0800

    [feat]: 添加新的屏蔽关键条目
    -

--- jin10-futures-assistant.js
+++ jin10-futures-assistant.js
@@ -25,32 +25,44 @@
         "期货盯盘神器",
         "交易夜读",
         "订单流5分钟图显示",
+        "订单流15分钟图显示",
         "热门品种观点分享",
         "量价分布与订单流复盘",
         "技术分析",
         "资金炸弹复盘",
-        "品种交易逻辑"
+        "品种交易逻辑",
+        "期货观点汇总"
     ];
     var blacklist_image_links = [
-        "https://cdn-news.jin10.com/assets/common/13679271-9e9e-4b4f-8143-03a5524c78e9.jpg",
-        "https://cdn-news.jin10.com/assets/common/33e9e3d7-9484-47e7-bed2-f64d7f2c4e48.jpg",
-        "https://cdn-news.jin10.com/assets/common/52f3ddff-d53c-488a-8370-9f62d68516f6.jpg",
-        "https://cdn-news.jin10.com/assets/common/40d04d52-951b-4132-a0b9-7474ae814efe.jpg",
-        "https://cdn-news.jin10.com/assets/common/e6bcdc1e-a911-4346-be23-5261c5f5156d.jpg",
-        "https://cdn-news.jin10.com/assets/common/c0624356-4e17-4136-a4d4-f50801f82b30.jpg",
-        "https://cdn-news.jin10.com/assets/common/317c5dce-0065-47d5-af04-6bd9a6a210a5.jpg",
-        "https://cdn-news.jin10.com/assets/common/73d61e20-bb54-44cc-87fa-43a9defc308f.jpg",
-        "https://cdn-news.jin10.com/assets/common/2d235cbd-f851-4e40-b16b-f72bdd72a7fb.jpg",
-        "https://cdn-news.jin10.com/assets/common/7ac16be9-232f-4279-bc33-5040a8a97862.jpg",
-        "https://cdn-news.jin10.com/assets/common/17659d35-bb8b-4f59-ab97-4c2f7c8567a0.jpg",
-        "https://cdn-news.jin10.com/assets/common/4c118b32-334f-4287-b41b-8a0d1177598d.jpg",
-        "https://cdn-news.jin10.com/assets/common/e6bcdc1e-a911-4346-be23-5261c5f5156d.jpg",
-        "https://cdn-news.jin10.com/assets/common/1e9e7eb1-cd6e-4609-8df1-57b69f5c8470.jpg",
-        "https://cdn-news.jin10.com/assets/common/41195501-edba-4e8d-b4e5-9887c338c45c.jpg",
-        "https://cdn-news.jin10.com/assets/common/978fa203-b4b0-4e19-a4dc-b5fe89cc7e68.jpg",
-        "https://cdn-news.jin10.com/assets/common/318ef326-a414-4cb2-b89d-d51ab6e6350f.jpg",
-        "https://cdn-news.jin10.com/assets/common/fd578307-fa2a-4b19-8551-73657c357bcd.jpg",
-        "https://cdn-news.jin10.com/assets/common/aa0df400-b3d9-4412-8921-6664d82f3f3a.jpg",
+        "https://cdn-news.jin10.com/assets/common/c0624356-4e17-4136-a4d4-f50801f82b30.jpg",// 沪铜
+        "https://cdn-news.jin10.com/assets/common/df0ebd52-7eb1-457c-a3e5-95dd648e9698.jpg",// 沪铝
+        "https://cdn-news.jin10.com/assets/common/a2bc392c-469c-4d4c-b8ca-96c399c8116a.jpg",// 沪镍
+        "https://cdn-news.jin10.com/assets/common/d4ac9f34-09da-44c8-9dac-63c5a667c1ab.jpg",// 氧化铝
+        "https://cdn-news.jin10.com/assets/common/317c5dce-0065-47d5-af04-6bd9a6a210a5.jpg",// 原油
+        "https://cdn-news.jin10.com/assets/common/e93d9707-3407-439e-8cbc-5c6dca9c3c07.jpg",// 燃料油
+        "https://cdn-news.jin10.com/assets/common/41195501-edba-4e8d-b4e5-9887c338c45c.jpg",// 铁矿石
+        "https://cdn-news.jin10.com/assets/common/a889cf66-4eb6-40b5-8d87-cc1a66f6d78c.jpg",// 焦煤
+        "https://cdn-news.jin10.com/assets/common/17659d35-bb8b-4f59-ab97-4c2f7c8567a0.jpg",// 螺纹钢
+        "https://cdn-news.jin10.com/assets/common/de4ba63c-8cbe-4af1-a4a6-0311d697bb19.jpg",// 锰硅
+        "https://cdn-news.jin10.com/assets/common/318ef326-a414-4cb2-b89d-d51ab6e6350f.jpg",// 碳酸锂
+        "https://cdn-news.jin10.com/assets/common/1e9e7eb1-cd6e-4609-8df1-57b69f5c8470.jpg",// 纯碱
+        "https://cdn-news.jin10.com/assets/common/40d04d52-951b-4132-a0b9-7474ae814efe.jpg",// 尿素
+        "https://cdn-news.jin10.com/assets/common/e6bcdc1e-a911-4346-be23-5261c5f5156d.jpg",// 玻璃
+        "https://cdn-news.jin10.com/assets/common/73d61e20-bb54-44cc-87fa-43a9defc308f.jpg",// 甲醇
+        "https://cdn-news.jin10.com/assets/common/7ac16be9-232f-4279-bc33-5040a8a97862.jpg",// PP
+        "https://cdn-news.jin10.com/assets/common/7093a3b1-8ee9-4845-8d9d-6795096aa2b2.jpg",// PVC
+        "https://cdn-news.jin10.com/assets/common/fd578307-fa2a-4b19-8551-73657c357bcd.jpg",// PTA
+        "https://cdn-news.jin10.com/assets/common/978fa203-b4b0-4e19-a4dc-b5fe89cc7e68.jpg",// 橡胶
+        "https://cdn-news.jin10.com/assets/common/33e9e3d7-9484-47e7-bed2-f64d7f2c4e48.jpg",// 豆粕
+        "https://cdn-news.jin10.com/assets/common/4200df63-143a-43ce-860d-233e1e1b73dd.jpg",// 菜粕
+        "https://cdn-news.jin10.com/assets/common/4c118b32-334f-4287-b41b-8a0d1177598d.jpg",// 棕榈油
+        "https://cdn-news.jin10.com/assets/common/aa0df400-b3d9-4412-8921-6664d82f3f3a.jpg",// 豆油
+        "https://cdn-news.jin10.com/assets/common/2d235cbd-f851-4e40-b16b-f72bdd72a7fb.jpg",// 菜油
+        "https://cdn-news.jin10.com/assets/common/13679271-9e9e-4b4f-8143-03a5524c78e9.jpg",// 棉花
+        "https://cdn-news.jin10.com/assets/common/e22be1ec-bf40-4ab0-af8b-77d943bdf108.jpg",// 白糖
+        "https://cdn-news.jin10.com/assets/common/35e7cfe2-8ab5-4e27-96b8-26d8c7615f09.jpg",// 玉米
+        "https://cdn-news.jin10.com/assets/common/6e3e562c-fd7d-4af5-ac91-7661850ce9ad.jpg",// 生猪
+        "https://cdn-news.jin10.com/assets/common/52f3ddff-d53c-488a-8370-9f62d68516f6.jpg",// 航运
         "https://flash-scdn.jin10.com/1375ec78-c99c-4aa1-a6bc-496d5d25df4c.png",
         "https://flash-scdn.jin10.com/85194ea0-b68c-49bd-8c61-c1208a3dac06.jpg"
     ];
